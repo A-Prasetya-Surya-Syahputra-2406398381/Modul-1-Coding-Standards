e0061890b86fe106a6436ac0bdbef2919f45157c
prasetyasurya-ui <prasetya.surya@ui.ac.id>
2026-02-13 08:43:09 +0700

    feat: update build.gradle.kts config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    jacoco
     java
     id("org.springframework.boot") version "3.5.10"
     id("io.spring.dependency-management") version "1.1.7"
@@ -64,4 +65,16 @@ tasks.register<Test>("functionalTest") {
 
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+}
+
+tasks.test {
+    filter {
+        excludeTestsMatching("*FunctionalTest")
+    }
+
+    finalizedBy("jacocoTestReport")
 }
